7afef75d467c149082f7b5ea6bb61fb4890a032e
Yannick <20306025+yandeu@users.noreply.github.com>
2020-05-22 01:42:37 +0200

    Added codecov

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  collectCoverage: true,
-  // collectCoverageFrom: ['packages/**/*.{js}', '!**/node_modules/**'],
-  coverageDirectory: 'output/coverage/jest',
-  preset: 'jest-puppeteer',
-  coverageReporters: ['text', 'cobertura'],
-  roots: ['test/']
+  preset: 'jest-puppeteer'
 }
