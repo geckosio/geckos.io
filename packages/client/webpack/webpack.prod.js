59fda8fd6321838177410861fd7927fd0c7698af
yandeu <20306025+yandeu@users.noreply.github.com>
2020-12-30 22:13:02 +0100

    Upgrade to Webpack 5

--- packages/client/webpack/webpack.prod.js
+++ packages/client/webpack/webpack.prod.js
@@ -4,13 +4,13 @@ const TerserPlugin = require('terser-webpack-plugin')
 /**
  * Make the minified bundle of the client lib
  */
-module.exports = (env, argv) => {
+module.exports = env => {
   return {
     mode: 'production',
     entry: path.resolve(__dirname, '../src/bundle.ts'),
     output: {
-      path: path.resolve(__dirname, `${argv.path}`),
-      filename: `geckos.io-client.${argv.packageVersion}.min.js`,
+      path: path.resolve(__dirname, `${env.path}`),
+      filename: `geckos.io-client.${env.packageVersion}.min.js`,
       library: 'geckos',
       libraryExport: 'default'
     },
