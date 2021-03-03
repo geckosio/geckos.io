b28d9e324d77b5314fb804a1bf0b1eaea4542139
yandeu <20306025+yandeu@users.noreply.github.com>
2021-03-03 22:48:49 +0100

    Export geckos not only as default

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -2,4 +2,5 @@ import client, { ClientChannel } from './geckos/channel'
 import { Data, RawMessage, ChannelId } from '@geckos.io/common/lib/types'
 
 export default client
+export { client as geckos }
 export type { ClientChannel, Data, RawMessage, ChannelId }
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,5 +3,5 @@ import server, { GeckosServer, ServerChannel } from './geckos/server'
 import { Data, RawMessage, ChannelId } from '@geckos.io/common/lib/types'
 
 export default server
-export { iceServers }
+export { server as geckos, iceServers }
 export type { GeckosServer, ServerChannel, Data, RawMessage, ChannelId }
