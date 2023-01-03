c631129de08c0b099cd30cfd4973b7fc8a4579bd
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2023-01-03 02:05:20 +0100

    Rework the test
    - Use different ports to avoid conflicts
    - Properly teardown the servers and pages
    - Use connectionsManager

--- test/e2e/multiplexing.test.js
+++ test/e2e/multiplexing.test.js
@@ -1,10 +1,13 @@
 /* eslint-disable sort-imports */
+import { afterThis } from 'jest-after-this'
 import { express, Static } from 'express6'
 import geckos from '../../packages/server/lib/index.js'
 import http from 'http'
 import path from 'path'
 
 import { __dirname } from './_dirname.js'
+
+const pause = t => new Promise(r => setTimeout(r, t))
 
 describe('Multiplexing (using a single UDP port)', () => {
   test('should work with multiplex', async () => {
@@ -19,56 +22,61 @@ describe('Multiplexing (using a single UDP port)', () => {
     io.addServer(server)
     server.listen(6969)
 
+    afterThis((done) => {
+      server.close(done)
+    })
+
     const firstPage = await browser.newPage()
     const secondPage = await browser.newPage()
     const thirdPage = await browser.newPage()
 
-    /** @type {Set<import("../../packages/server/lib/index.js").ServerChannel>} */
-    const connections = new Set()
-    io.onConnection(c => connections.add(c))
-
-    const allConnected = new Promise(r => {
-      const cID = setInterval(() => {
-        if (connections.size === 3) {
-          clearInterval(cID)
-          r()
-        }
-      }, 1000)
+    afterThis(async () => {
+      await firstPage.close()
+      await secondPage.close()
+      await thirdPage.close()
     })
 
     firstPage.goto('http://localhost:6969/e2e/multiplexing.html')
     secondPage.goto('http://localhost:6969/e2e/multiplexing.html')
     thirdPage.goto('http://localhost:6969/e2e/multiplexing.html')
-    await allConnected
-    expect(connections.size).toBe(3)
+
+    await pause(1000)
+
+    expect(io.connectionsManager.connections.size).toBe(3)
   })
 
   test('should NOT work without multiplex', async () => {
     const app = express()
     const server = http.createServer(app)
 
-    // If multiplexing is working, that single port should be enough for multiple clients.
-    const io = geckos({ portRange: { min: 20000, max: 20001 }, multiplex: false })
+    // Without multiplex, two ports should not be enough for all clients.
+    const io = geckos({ portRange: { min: 20002, max: 20004 }, multiplex: false })
 
     app.use('/', Static(path.join(__dirname, '../')))
 
     io.addServer(server)
-    server.listen(6969)
+    server.listen(6968)
+
+    afterThis((done) => {
+      server.close(done)
+    })
 
     const firstPage = await browser.newPage()
     const secondPage = await browser.newPage()
     const thirdPage = await browser.newPage()
 
-    /** @type {Set<import("../../packages/server/lib/index.js").ServerChannel>} */
-    const connections = new Set()
-    io.onConnection(c => connections.add(c))
+    afterThis(async () => {
+      await firstPage.close()
+      await secondPage.close()
+      await thirdPage.close()
+    })
 
-    const pause = t => new Promise(r => setTimeout(r, t))
+    firstPage.goto('http://localhost:6968/e2e/multiplexing.html')
+    secondPage.goto('http://localhost:6968/e2e/multiplexing.html')
+    thirdPage.goto('http://localhost:6968/e2e/multiplexing.html')
 
-    firstPage.goto('http://localhost:6969/e2e/multiplexing.html')
-    secondPage.goto('http://localhost:6969/e2e/multiplexing.html')
-    thirdPage.goto('http://localhost:6969/e2e/multiplexing.html')
-    await pause(2000)
-    expect(connections.size).toBe(2)
+    await pause(1000)
+
+    expect(io.connectionsManager.connections.size).toBe(2)
   })
 })
